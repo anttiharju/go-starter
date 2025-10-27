694174b9f858552e5a46ddfbc05c0db1ec913e6d
anttiharju <antti@harju.io>
2025-10-27 20:16:06 +0100

    Bring root files up-to-date with vmatch

--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"context"
 	"os"
 
-	"github.com/anttiharju/go-starter/internal/cli"
+	"github.com/anttiharju/go-starter/internal/buildinfo"
+	"github.com/anttiharju/go-starter/internal/choose"
 	"github.com/anttiharju/go-starter/internal/exitcode"
 	"github.com/anttiharju/go-starter/internal/interrupt"
+)
+
+var (
+	revision string
+	version  string
+	time     string
 )
 
 func main() {
 	go interrupt.Listen(exitcode.Interrupt, os.Interrupt)
 
 	ctx := context.Background()
-	exitcode := cli.Start(ctx, os.Args[1:])
-	os.Exit(int(exitcode))
+	exitCode := choose.Wrapper(ctx, buildinfo.New(revision, version, time), os.Args[1:])
+	os.Exit(exitCode)
 }
