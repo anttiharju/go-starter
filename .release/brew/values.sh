8be8291daa10aa25ab112e9a0b32da3895df3adb
Antti Harju <antti@harju.io>
2025-12-01 17:37:14 +0200

    Consistency

--- .release/brew/values.sh
+++ .release/brew/values.sh
@@ -20,8 +20,8 @@ capture PKG_OWNER "${GITHUB_REPOSITORY%%/*}"
 if [[ "$TAG" = "v0.0.0" ]]; then
   capture PKG_MAC_INTEL_SHA TBD
   capture PKG_MAC_ARM_SHA TBD
-  capture PKG_LINUX_INTEL_SHA TBD
   capture PKG_LINUX_ARM_SHA TBD
+  capture PKG_LINUX_INTEL_SHA TBD
   exit 0
 fi
 
@@ -36,7 +36,7 @@ mac_intel_sha="$(hashsum --sha256 "$repo-darwin-amd64.tar.gz" | cut -d ' ' -f1)"
 capture PKG_MAC_INTEL_SHA "$mac_intel_sha"
 mac_arm_sha="$(hashsum --sha256 "$repo-darwin-arm64.tar.gz" | cut -d ' ' -f1)"
 capture PKG_MAC_ARM_SHA "$mac_arm_sha"
-linux_intel_sha="$(hashsum --sha256 "$repo-linux-amd64.tar.gz" | cut -d ' ' -f1)"
-capture PKG_LINUX_INTEL_SHA "$linux_intel_sha"
 linux_arm_sha="$(hashsum --sha256 "$repo-linux-arm64.tar.gz" | cut -d ' ' -f1)"
 capture PKG_LINUX_ARM_SHA "$linux_arm_sha"
+linux_intel_sha="$(hashsum --sha256 "$repo-linux-amd64.tar.gz" | cut -d ' ' -f1)"
+capture PKG_LINUX_INTEL_SHA "$linux_intel_sha"
